6b8d964688f897244c3185c41d3862fdd1bd1689
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-03-28 23:38:46 +0300

    counter redux translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Counter from "./components/Counter/Counter";
 import Settings from "./components/Settings/Settings";
