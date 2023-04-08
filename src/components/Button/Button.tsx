f059faab3177dc26b4fed945ad7414a9b1b016e8
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-04-09 00:37:16 +0300

    counter redux translation

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,11 @@
 import React, {FC} from 'react';
 import s from './Button.module.css'
 import {Button} from "@mui/material";
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import PlusOneIcon from '@mui/icons-material/PlusOne';
 
 export type ButtonPropsType = {
-    name: string
+    name: 'set' | 'inc' | 'reset'
     callback: () => void
     disableIncButton?: boolean
     disableResetButton?: boolean
@@ -11,7 +13,7 @@ export type ButtonPropsType = {
     isButtonNotClicked: boolean
 }
 
-const MyButton:FC<ButtonPropsType> = React.memo((
+const MyButton: FC<ButtonPropsType> = React.memo((
     {
         name,
         callback,
@@ -24,8 +26,21 @@ const MyButton:FC<ButtonPropsType> = React.memo((
 
     const isDisable = disableSetButton || isButtonNotClicked || disableIncButton || disableResetButton
 
+    const getIcon = (name: 'set' | 'inc' | 'reset') => {
+        switch (name) {
+            case "set":
+                return 'set'
+            case "inc":
+                return <PlusOneIcon/>
+            case "reset":
+                return <RestartAltIcon/>
+        }
+    }
+
     return (
-        <Button className={s.button} variant={'outlined'} disabled={isDisable} onClick={callback}>{name}</Button>
+        <Button className={s.button} variant={'outlined'} disabled={isDisable} onClick={callback}>
+            {getIcon(name)}
+        </Button>
     );
 });
 
